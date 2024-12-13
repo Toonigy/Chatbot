e1b7760800d0fa0a1a52b56411d6e1871f450f80
Toonigy <62924215+Toonigy@users.noreply.github.com>
2024-12-12 18:41:43 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("signup-button").addEventListener("click", () => {
+  window.location.href = "signup.html"; // Link to the Sign-Up page
+});
+
+document.getElementById("login-button").addEventListener("click", () => {
+  window.location.href = "login.html"; // Link to the Login page
+});
